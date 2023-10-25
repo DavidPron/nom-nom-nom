14d94f00263ae1d70f8731ac33075db453916f59
David Pron <92371541+DavidPron@users.noreply.github.com>
2023-10-25 22:37:34 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+class VirtualPet {
+	constructor(name) {
+		this.name = name;
+	}
+	eat(food) {
+		if (food === "treats") {
+			console.log("nom nom nom");
+		} else {
+			console.log("discard");
+		}
+	}
+}
+const pet = new VirtualPet("Tom");
+pet.eat("treats");
